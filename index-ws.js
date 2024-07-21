a863da16fd714d277c1f98f89256093ec7998c2e
Adam782 <asteele2@steeledesign.dev>
2024-07-21 16:41:06 -0400

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -6,6 +6,14 @@ app.get("/", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
 server.on("request", app);
 server.listen(3000, function () {
   console.log("server running on port 3000");
@@ -25,6 +33,9 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+          VALUES (${numClients} datetime('now'))`);
+
   ws.on("close", function close() {
     wss.broadcast(`Current vistors ${numClients}`);
     console.log("A client has disconnected");
@@ -36,3 +47,26 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down db");
+  db.close();
+}
